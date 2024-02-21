ab16d89eac0a9a6fb57b748889d96ed8106ce263
SamarAhar <Tusharahar1906@gmail.com>
2024-02-21 07:11:44 +0530

    Code added

---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiVisualstudiocode,
+  SiPostman,
+  // SiSlack,
+  SiVercel,
+  SiWindows,
+  SiMongodb,
+} from "react-icons/si";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiWindows />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVercel />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
